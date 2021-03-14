24cd4f2fd4e78dce7242554fc14835bb39e98e5d
aaschlote <aaschlote@gmail.com>
2021-03-14 16:23:44 +0100

    routing learns

--- http-01-starting-setup/http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
+++ http-01-starting-setup/http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
@@ -8,14 +8,26 @@ class FullPost extends Component {
     loadedPost: null,
   };
 
+  componentDidMount() {
+    this.loadData();
+  }
+
   componentDidUpdate() {
-    if (this.props.id) {
+    this.loadData();
+  }
+
+  loadData() {
+    if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
-        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+        (this.state.loadedPost &&
+          this.state.loadedPost.id !== +this.props.match.params.id)
       ) {
         axios
-          .get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+          .get(
+            'https://jsonplaceholder.typicode.com/posts/' +
+              this.props.match.params.id
+          )
           .then((response) => {
             this.setState({ loadedPost: response.data });
           });
@@ -25,7 +37,10 @@ class FullPost extends Component {
 
   deletePostHandler = () => {
     axios
-      .delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      .delete(
+        'https://jsonplaceholder.typicode.com/posts/' +
+          this.props.match.params.id
+      )
       .then((response) => {
         console.log(response);
       });
@@ -34,7 +49,7 @@ class FullPost extends Component {
   render() {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
 
-    if (this.props.id) {
+    if (this.props.match.params.id) {
       post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
     }
 
